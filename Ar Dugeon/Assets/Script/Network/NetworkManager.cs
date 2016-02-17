2ddbb1a8a576a2e55293c7844eb44443b2f640d6
ravonyx <ravonyxa@gmail.com>
2016-02-17 22:56:05 +0100

    Lobby Scene List Room + Wait Room (Master view/Client view)

---
+++ Ar Dugeon/Assets/Script/Network/NetworkManager.cs
@@ -0,0 +1,60 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using Photon;
+
+public class NetworkManager : Photon.PunBehaviour
+{
+
+    public Text connectionParameters;
+    public Text roomName;
+    public bool inRoom = false;
+    public Canvas canvas;
+    private ListRooms _listRooms;
+
+    void Start ()
+    {
+        PhotonNetwork.ConnectUsingSettings("0.1");
+        PhotonNetwork.logLevel = PhotonLogLevel.Full;
+        _listRooms = canvas.GetComponent<ListRooms>();
+    }
+
+	void Update ()
+    {
+        connectionParameters.text = PhotonNetwork.connectionStateDetailed.ToString();
+    }
+
+    public override void OnJoinedLobby()
+    {
+        //PhotonNetwork.JoinRandomRoom();
+    }
+
+    public void CreateRoom()
+    {
+        if(!inRoom)
+            PhotonNetwork.CreateRoom(roomName.text, new RoomOptions() { maxPlayers = 4 }, null);
+        inRoom = true;
+    }
+
+    public override void OnReceivedRoomListUpdate()
+    {
+        Debug.Log("OnreceiveRoomList");
+    }
+
+    void OnPhotonRandomJoinFailed()
+    {
+        Debug.Log("Can't join random room!");
+    }
+
+    public void JoinRoom()
+    {
+        if(!inRoom)
+            PhotonNetwork.JoinRoom(_listRooms.selectedRoomName);
+        inRoom = true;
+    }
+    
+    public override void OnJoinedRoom()
+    {
+
+    }
+}
---
+++ Ar Dugeon/Assets/Script/Network/UIManagerLobby.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIManagerLobby : MonoBehaviour
+{
+    private NetworkManager networkScript;
+    public GameObject connectionPanel;
+    public GameObject waitPanel;
+
+    public GameObject masterPanel;
+    public Text NbPlayers;
+	// Use this for initialization
+	void Start ()
+    {
+        networkScript = GetComponent<NetworkManager>();
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        int nbPlayers = PhotonNetwork.playerList.Length;
+        NbPlayers.text = nbPlayers + "/4";
+        if (networkScript.inRoom)
+        {
+            connectionPanel.SetActive(false);
+            waitPanel.SetActive(true);
+        }
+        else
+        {
+            connectionPanel.SetActive(true);
+            waitPanel.SetActive(false);
+        }
+
+        if (PhotonNetwork.isMasterClient)
+            masterPanel.SetActive(true);
+        else 
+            masterPanel.SetActive(false);
+    }
+}
